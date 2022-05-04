cb97f61fd9b2f4b1c51121ad3c7ca35e18d5f957
Filipe Motta <femotta96@gmail.com>
2022-05-04 19:36:08 -0300

    Aulas 1, 2 e 3 completed

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "3071961ed3df63",
+    pass: "6484d2ea02f800",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Filipe Motta <femotta96@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
